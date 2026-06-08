47da647bed36dcfdfdc4b2cfbb42fd51868b23aa
ksk <shivakrishna162209@gmail.com>
2026-06-08 15:40:51 +0530

    I am changing the seconds to 120

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ from datetime import timedelta
 class Config:
     SQLALCHEMY_DATABASE_URI = "mysql://root:root@localhost/project"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=60)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(seconds=120)
     JWT_SECRET_KEY = "jwt_secret_key"
 
